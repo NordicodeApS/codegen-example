5941855b09eeaf10ed1628d646adfe60562e94bf
Kevin <98403145+kevin-nordicode@users.noreply.github.com>
2023-12-18 14:31:14 +0100

    Migration for Ticket

---
+++ output/rails/db/migrate/20220401123500_create_tickets.rb
@@ -0,0 +1,10 @@
+class CreateTickets < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tickets do |t|
+      t.integer :seat
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
