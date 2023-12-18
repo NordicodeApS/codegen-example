a6c9d926bbed522ccd152c018e9bebeceabdece1
Kevin <98403145+kevin-nordicode@users.noreply.github.com>
2023-12-18 15:49:03 +0100

    Controller for Token model

---
+++ output/rails/app/controllers/tokens_controller.rb
@@ -0,0 +1,25 @@
+class TokensController < ApplicationController
+
+  def create
+    @token = Token.new(token_params)
+
+    if @token.save
+      render :json => @token, status: :created
+    else
+      render :json => { errors: @token.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @token = Token.find(params[:id])
+    @token.destroy
+
+    render :json => {}, status: :no_content
+  end
+
+  private
+  def token_params
+    params.require(:token).permit(:email, :password)
+  end
+
+end
