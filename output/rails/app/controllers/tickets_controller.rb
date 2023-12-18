ccd7797b9ef1c87d9959fc13def0f222647e0637
Kevin <98403145+kevin-nordicode@users.noreply.github.com>
2023-12-18 14:53:11 +0100

    Controller for Ticket endpoints

---
+++ output/rails/app/controllers/tickets_controller.rb
@@ -0,0 +1,35 @@
+class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.where(show_id: params[:show_id])
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      render json: @ticket, status: :created
+    else
+      render json: @ticket.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @ticket = Ticket.find(params[:id])
+
+    if @ticket.update(ticket_params)
+      render json: @ticket
+    else
+      render json: @ticket.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @ticket = Ticket.find(params[:id])
+    @ticket.destroy
+  end
+
+  private
+  def ticket_params
+    params.require(:ticket).permit(:seat, :available)
+  end
+end
