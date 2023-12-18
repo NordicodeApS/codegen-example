b8189c053ac7aa141f508297c4df07db7b30806c
Kevin <kevin@nordicode.dk>
2023-12-18 13:01:08 +0100

    Change: Set indentation for article_test

--- examples/rails/test/article_test.rb
+++ examples/rails/test/article_test.rb
@@ -2,23 +2,23 @@ require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
     test 'should be valid with valid attributes' do
-      article = build(:article)
-      assert article.valid?
+        article = build(:article)
+        assert article.valid?
     end
-  
+
     test 'should not be valid without a title' do
-      article = build(:article, title: nil)
-      assert_not article.valid?
+        article = build(:article, title: nil)
+        assert_not article.valid?
     end
-  
+
     test 'should not be valid without content' do
-      article = build(:article, content: nil)
-      assert_not article.valid?
+        article = build(:article, content: nil)
+        assert_not article.valid?
     end
-  
+
     test 'should have many comments' do
-      article = create(:article)
-      comment = create(:comment, article: article)
-      assert_includes article.comments, comment
+        article = create(:article)
+        comment = create(:comment, article: article)
+        assert_includes article.comments, comment
     end
-  end
+end
