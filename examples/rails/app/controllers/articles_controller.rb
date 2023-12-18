c8d72cd26a24bc98976a0bb5bc1bd411d43cb025
Kevin <kevin@nordicode.dk>
2023-12-18 14:50:54 +0100

    Change: Modify routes and controller example

--- examples/rails/app/controllers/articles_controller.rb
+++ examples/rails/app/controllers/articles_controller.rb
@@ -1,37 +1,37 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+        render :json => ArticleSerializer.serialize(Article.all)
     end
 
     def show
-        @article = Article.find(params[:id])
+        render :json => ArticleSerializer.serialize(Article.find(params[:id]))
     end
 
     def new
-        @article = Article.new
+        render :json => ArticleSerializer.serialize(Article.new)
     end
 
     def create
         @article = Article.new(article_params)
 
         if @article.save
-            redirect_to @article
+            render :json => ArticleSerializer.serialize(@article), status: :created
         else
-            render :new, status: :unprocessable_entity
+            render :json => { errors: @article.errors }, status: :unprocessable_entity
         end
     end
 
     def edit
-        @article = Article.find(params[:id])
+        render :json => ArticleSerializer.serialize(Article.find(params[:id]))
     end
 
     def update
         @article = Article.find(params[:id])
 
         if @article.update(article_params)
-            redirect_to @article
+            render :json => ArticleSerializer.serialize(@article), status: :ok
         else
-            render :edit, status: :unprocessable_entity
+            render :json => { errors: @article.errors }, status: :unprocessable_entity
         end
     end
 
@@ -39,7 +39,7 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
         @article.destroy
     
-        redirect_to root_path, status: :see_other
+        render :json => {}, status: :no_content
     end
 
     private
--- examples/rails/config/routes.rb
+++ examples/rails/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     get "/articles", to: "articles#index"
     get "/articles/:id", to: "articles#show"
     post "/articles", to: "articles#create"
+    put "/articles/:id", to: "articles#update"
+    delete "/articles/:id", to: "articles#destroy"
 
-    resources :comments
 end
